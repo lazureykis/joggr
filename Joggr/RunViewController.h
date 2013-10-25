f0b86ec6e38139493c5bfcc04625e34b2fef382d
Pavel Lazureykis <lazureykis@gmail.com>
2013-10-25 21:04:07 +0400

    run view controller

---
+++ Joggr/RunViewController.h
@@ -0,0 +1,13 @@
+//
+//  RunViewController.h
+//  Joggr
+//
+//  Created by Павел Лазурейкис on 25.10.13.
+//  Copyright (c) 2013 Павел Лазурейкис. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RunViewController : UIViewController
+
+@end
---
+++ Joggr/RunViewController.m
@@ -0,0 +1,38 @@
+//
+//  RunViewController.m
+//  Joggr
+//
+//  Created by Павел Лазурейкис on 25.10.13.
+//  Copyright (c) 2013 Павел Лазурейкис. All rights reserved.
+//
+
+#import "RunViewController.h"
+
+@interface RunViewController ()
+
+@end
+
+@implementation RunViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
