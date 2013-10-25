efebfc865ef69533fc83c564462bdfc3582fc3ff
Pavel Lazureykis <lazureykis@gmail.com>
2013-10-25 20:41:11 +0400

    Initial Commit

---
+++ Joggr/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Joggr
+//
+//  Created by Павел Лазурейкис on 25.10.13.
+//  Copyright (c) 2013 Павел Лазурейкис. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Joggr/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Joggr
+//
+//  Created by Павел Лазурейкис on 25.10.13.
+//  Copyright (c) 2013 Павел Лазурейкис. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
