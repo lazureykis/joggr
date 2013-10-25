975d36f225ca6c4619a1a4c53987982bc6f543af
Pavel Lazureykis <lazureykis@gmail.com>
2013-10-25 20:50:52 +0400

    buttons

--- Joggr/AppDelegate.h
+++ Joggr/AppDelegate.h
@@ -1,6 +1,7 @@
 //
 //  AppDelegate.h
 //  Joggr
+// https://developer.apple.com/wwdc/videos/index.php?id=712
 //
 //  Created by Павел Лазурейкис on 25.10.13.
 //  Copyright (c) 2013 Павел Лазурейкис. All rights reserved.
